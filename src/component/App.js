a9c032a597f4cd6756778decc0ef2d9c6b4e3392
Gleb Phokov <phokov@ya.ru>
2018-03-09 13:22:32 +0300

    FRONT_16: Initial

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+  Search,
+  Container,
+  Sidebar,
+  Menu,
+  Icon
+} from 'semantic-ui-react'
+
+import TaskList from './TaskList'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarVisible: false,
+      AddEventDisplay: ""
+    };
+  }
+
+  toggleVisibility = () => this.setState({ sidebarVisible: !this.state.sidebarVisible });
+  toggleAddEvent = () => this.setState({
+    AddEventDisplay: this.state.AddEventDisplay === "block" ? "none" : "block"
+  });
+
   render() {
     return (
       <div id="wrapper">
-          Mock.
+          <Sidebar as={Menu} visible={this.state.sidebarVisible} vertical>
+            <Menu.Item name='item1'>
+              Item 1
+            </Menu.Item>
+            <Menu.Item name='item2'>
+              Item 2
+            </Menu.Item>
+            <Menu.Item name='item3'>
+              Item 3
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Container style={{ marginTop: '1em' }}>
+              <div className="top-bar">
+                <Icon className="left-float" name='content' onClick={this.toggleVisibility} />
+                <Search
+                  className="search-with-buttons"
+                  size="mini"
+                  input={{ fluid: true }}
+                />
+                <Icon name='plus' className="right-float" onClick={this.toggleAddEvent}/>
+              </div>
+              <div className="main-list">
+                <TaskList />
+              </div>
+            </Container>
+          </Sidebar.Pusher>
       </div>
     );
   }
