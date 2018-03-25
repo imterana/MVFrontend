dca624a8e90614af7e0d92bb4128668fc6d84b45
AFakeman <anton_suslov@me.com>
2018-03-26 00:36:07 +0300

    FRONT_52: Lint fixes

--- src/App/Navigation/NavigationScreen.js
+++ src/App/Navigation/NavigationScreen.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {StyleSheet, View} from 'react-native';
 import {withRouter} from 'react-router-dom';
 
-import { Button } from '../Components';
+import {Button} from '../Components';
 
 import {NavigationBar} from './NavigationBar';
 import SideBar from './SideBar';
