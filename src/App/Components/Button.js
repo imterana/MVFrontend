fcbaca65b6f19e6d5cd17fc107e4ba0983f14b2b
Shiyanov Vadim <vadimsh853@gmail.com>
2018-04-01 22:17:15 +0300

    FRONT_61: add Button component

---
+++ src/App/Components/Button.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Text, TouchableOpacity} from 'react-native';
+
+/**
+ * A custom button used for most buttons in project.
+ * Contains text. Can have only one of three default styles.
+ */
+export default class StandartButton extends Component {
+  static propTypes = {
+    /**
+     * Label can only be a string.
+     */
+    label: PropTypes.string,
+    /**
+     * Press handler function.
+     */
+    onPress: PropTypes.func,
+    /**
+     * A style can only be one of three default.
+     * It is passed via string, containig its name.
+     */
+    styleName: PropTypes.string,
+  }
+
+  static defaultProps = {
+    styleName: null,
+  }
+
+  /**
+   * @return {React.Node} A styled button component.
+   */
+  render() {
+    const {onPress, styleName} = this.props;
+    if (styleName == 'baseButton'
+      || !(styleName in style)) {
+      return <Text>bad</Text>;
+    }
+    const buttonStyle = [style.baseButton, style[styleName]];
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        style={buttonStyle}>
+        {this.props.label}
+      </TouchableOpacity>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  baseButton: {
+    height: 50,
+    flex: 1,
+  },
+  blueBack: {
+    backgroundColor: 'blue',
+  },
+  greenBack: {
+    backgroundColor: 'green',
+  },
+  redBack: {
+    backgroundColor: 'red',
+  },
+});
--- src/App/Components/index.js
+++ src/App/Components/index.js
@@ -1,2 +1,3 @@
 export {default as IconSymbol} from './IconSymbol';
 export {default as NavButton} from './NavButton';
+export {default as Button} from './Button'
