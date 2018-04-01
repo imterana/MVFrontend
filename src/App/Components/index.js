b65721fbfc793ad1ee95dc453fa110c8df2336d5
Shiyanov Vadim <vadimsh853@gmail.com>
2018-04-01 22:36:00 +0300

    FRONT_61: fix missing semicolon

--- src/App/Components/index.js
+++ src/App/Components/index.js
@@ -1,3 +1,3 @@
 export {default as IconSymbol} from './IconSymbol';
 export {default as NavButton} from './NavButton';
-export {default as Button} from './Button'
+export {default as Button} from './Button';
