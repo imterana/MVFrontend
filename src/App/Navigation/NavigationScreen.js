a04f065bb60a5ac054dbd1ac6808db56c1e43e39
Shiyanov Vadim <vadimsh853@gmail.com>
2018-04-02 18:45:08 +0300

    FRONT_61: resolve eslint failure

--- src/App/Navigation/NavigationScreen.js
+++ src/App/Navigation/NavigationScreen.js
@@ -5,7 +5,6 @@ import {withRouter} from 'react-router-dom';
 
 import {NavButton} from '../Components';
 
-import StyleConstants from '../StyleConstants';
 import {NavigationBar} from './NavigationBar';
 import SideBar from './SideBar';
 
