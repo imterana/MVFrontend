085dcab0f0010c63ff66f500b2ff9a5a765fcb2d
Shiyanov Vadim <vadimsh853@gmail.com>
2018-04-02 12:38:54 +0300

    FRONT_61: add limiting container for first button and top property to all containers

--- src/App/Screens/ButtonScreen.js
+++ src/App/Screens/ButtonScreen.js
@@ -34,41 +34,45 @@ export default class ButtonScreen extends Component {
     onPressFunction = this.onButtonPress.bind(this);
     return (
       <NavigationScreen>
+        <View style={style.buttonContainer}>
           <Button
             label='button'
             onPress={onPressFunction}
             background='blue' />
-          <View style={style.countContainer}>
-            <Text>
-              {this.state.count !== 0 ? this.state.count: null}
-            </Text>
-          </View>
-          <View style={style.rowButtons}>
-            <Button
-              label='button1'
-              onPress={onPressFunction}
-              background='green' />
-            <Button
-              label='button2'
-              onPress={onPressFunction}
-              background='red' />
-          </View>
+        </View>
+        <View style={style.countContainer}>
+          <Text>
+            {this.state.count !== 0 ? this.state.count: null}
+          </Text>
+        </View>
+        <View style={style.rowButtons}>
+          <Button
+            label='button1'
+            onPress={onPressFunction}
+            background='green' />
+          <Button
+            label='button2'
+            onPress={onPressFunction}
+            background='red' />
+        </View>
       </NavigationScreen>
     );
   }
 }
 
 const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  buttonContainer: {
+    top: 10,
+    flex: 0,
+    height: 50,
   },
   countContainer: {
+    top: 10,
     alignItems: 'center',
     padding: 10,
   },
   rowButtons: {
+    top: 10,
     flex: 1,
     flexDirection: 'row',
   },
