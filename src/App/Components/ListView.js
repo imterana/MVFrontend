7cbbfae92657fcb9c34da6c37eee6d1906c1b299
DamirFrontgod <sherbet.box@mail.ru>
2018-05-04 19:17:00 +0300

    FRONT_60: added styles

--- src/App/Components/ListView.js
+++ src/App/Components/ListView.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {ListView, Text, TouchableOpacity, View} from 'react-native';
+import {ListView, Text, TouchableOpacity, View, StyleSheet} from 'react-native';
 import StyleConstants from '../StyleConstants';
 import {IconSymbol} from '../Components';
 
@@ -22,19 +22,18 @@ class ListRow extends Component {
     const {title, subtitle, onPress} = this.props;
     return (
       <TouchableOpacity onPress={onPress}>
-        <View style={{flexDirection: 'row', height: 65, marginLeft: 10, marginRight: 10, marginTop: 10, borderRadius: 5, borderColor: StyleConstants.BORDER_COLOR,
-    borderWidth: 1, backgroundColor: 'white'}}>
-          <View style={{backgroundColor: StyleConstants.ALT_BACKGROUND_COLOR, width: 5, borderRadius: 5}}/>
-          <View style={{flexDirection: 'column', marginLeft: 10, marginTop: 8, flex: 0.99}}> 
-            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
+        <View style={styles.rowContainer}>
+          <View style={styles.stick}/>
+          <View style={styles.infoContainer}> 
+            <View style={styles.title}>
               <Text>{title}</Text>
             </View>
-            <View style={{flex: 1, flexDirection: 'column'}}>
+            <View style={styles.subtitle}>
               <Text style={{color: 'grey'}}>{subtitle}</Text>
             </View>  
           </View>
-          <View style={{flex: 0, flexDirection: 'column', justifyContent: 'center', marginRight: 20}}>
-            <IconSymbol name='circle-right' style={{color: StyleConstants.ALT_BACKGROUND_COLOR}}/>
+          <View style={styles.iconContainer}>
+            <IconSymbol name='circle-right' style={styles.icon}/>
           </View>
         </View>
       </TouchableOpacity>
@@ -57,7 +56,7 @@ class SectionHeader extends Component {
   render() {
     const {title} = this.props;
     return (
-      <View style={{flex: 1, flexDirection: 'column', height: 50}}>
+      <View style={styles.headerContainer}>
         <Text style={{color: 'grey'}}>{title}</Text>
       </View> 
     );
@@ -85,3 +84,51 @@ export default class DefaultListView extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  rowContainer: {
+    flexDirection: 'row', 
+    height: 65, 
+    marginLeft: 10, 
+    marginRight: 10, 
+    marginTop: 10, 
+    borderRadius: 5, 
+    borderColor: StyleConstants.BORDER_COLOR,
+    borderWidth: 1, 
+    backgroundColor: 'white',
+  },
+  stick: {
+    backgroundColor: StyleConstants.ALT_BACKGROUND_COLOR, 
+    width: 5, 
+    borderRadius: 5,
+  },
+  infoContainer: {
+    flexDirection: 'column', 
+    marginLeft: 10, 
+    marginTop: 8, 
+    flex: 1,
+  },
+  title: {
+    flex: 1, 
+    flexDirection: 'column', 
+    justifyContent: 'center',
+  },
+  subtitle: {
+    flex: 1, 
+    flexDirection: 'column',
+  },
+  iconContainer: {
+    flex: 0, 
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    marginRight: 20,
+  },
+  icon: {
+    color: StyleConstants.ALT_BACKGROUND_COLOR,
+  },
+  headerContainer: {
+    flex: 1, 
+    flexDirection: 'column', 
+    height: 50,
+  },
+});
